8902eea4ef5aa0142ccd5040cb2b1883c8cff50b
Anubhav-code-in-whjr <80476846+Anubhav-code-in-whjr@users.noreply.github.com>
2021-10-15 13:01:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+canvas = 
+         document.getElementById("myCanvas");
+         ctx=canvas.getContext('2d');
+canvas.addEventListener("mouseEvent", my_mouseEvent);
+canvas.addEventListener("mousedown", my_mousedown);
+function my_mousedown(e)
+{ color = document.getElementById("color").value;
+ radius = document.getElementById("radius").value;
+ line_width = document.getElementById("line-width").value;
+mouseEvent = "mouseDown"};
+canvas.addEventListener("mousemove", my_mousemove);
+function my_mousemove(e)
+{
+    current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
+    current_position_of_mouse_y = e.clienty - canvas.offsetTop;
+
+    if (mouseEvent == "mouseDown") {
+console.log("Current position of x and y coordinates =");
+console.log("x = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y)
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = line_width;
+        ctx.arc(current_position_of_mouse_x, current_position_of_mouse_y, radius, 0 , 2 * Math.PI);
+        ctx.stroke();
+    }
+}
